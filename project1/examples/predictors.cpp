630f71213f7fe3f539a913b17d4cc50d5883f183
MGomez99 <xmgomez2012@gmail.com>
2019-10-21 00:07:59 -0400

    wip

--- project1/examples/predictors.cpp
+++ project1/examples/predictors.cpp
@@ -1,57 +1,107 @@
 #include "predictors.h"
 //#define FILEPATH "/traces/short_trace1.txt"
-#define FILEPATH "test_input.txt"
+#define INPUTFILEPATH "short_trace1.txt"
+#define OUTPUTFILEPATH "test_output.txt"
 using namespace std;
 
 void alwaysTaken(unsigned long long address, string behavior, unsigned long long target, int &correct){
-    //cout << "ALWAYS TAKEN:\t";
-    //cout << address << "\t Prediction: T" << "\tActual: ";
+
     if(behavior == "T"){
-        //cout << "T"<<endl;
         correct++;
     }
-    else if (behavior == "NT"){
-        //cout << "NT" <<endl;
-    }
-    else{
-        cout << "err with parse"<<endl;
-    }
+    else if (behavior == "NT"){}
+    else{cout << "err with parse"<<endl;}
 }
 
 void alwaysNonTaken(unsigned long long address, string behavior, unsigned long long target, int &correct){
-    //cout << "ALWAYS NON TAKEN:\t";
-    //cout << address << "\t Prediction: NT" << "\tActual: ";
+
     if(behavior == "NT"){
-        //cout << "NT"<<endl;
         correct++;
     }
-    else if (behavior == "T"){
-        //cout << "T" <<endl;
-    }
-    else{
-        cout << "err with parse"<<endl;
-    }
+    else if (behavior == "T"){}
+    else{cout << "err with parse"<<endl;}
 }
 
 void predictor(){
-
+    /*********************************************************************/
+    //Parsing Variables
     unsigned long long address = 0;
     unsigned long long target = 0;
     string behavior;
     string line;
-
+    /*********************************************************************/
+    //ALWAYS TAKEN & ALWAYS NON TAKEN
     int atpredictions = 0;
     int antpredictions = 0;
-
-    ifstream infile(FILEPATH);
+    /*********************************************************************/
+    //BIMODAL CLASSES
+    vector<Bimodal> singleBitBimodal;
+    vector<Bimodal> doubleBitBimodal;
+    singleBitBimodal.resize(8);
+    doubleBitBimodal.resize(8);
+    //Sizes 16 32 64 128 256 512 1024 2048
+    int sizes[7] = {16, 32, 128, 256, 512, 1024, 2048};
+    for(int i = 0; i < 7; i++){
+        singleBitBimodal[i] = Bimodal(sizes[i], 1);
+        doubleBitBimodal[i] = Bimodal(sizes[i], 2);
+    }
+    /*********************************************************************/
+    //GSHARE CLASSES
+    vector<GShare> GShares;
+    GShares.resize(9);
+    for(int i = 0; i < 9; i++){
+        GShares[i] = GShare(3+i);
+    }
+    //3 Bits to 11 Bits
+    /*********************************************************************/
+    //Predictions
+    ifstream infile(INPUTFILEPATH);
+    int branches = 0;
     while(getline(infile, line)){
+        ++branches;
         stringstream s(line);
         s >> std::hex >> address >> behavior >> std::hex >> target;
         alwaysTaken(address, behavior, target, atpredictions);
         alwaysNonTaken(address, behavior, target, antpredictions);
-    }
-    cout << "AT Correct: "<<atpredictions<<"\tNT Correct: "<< antpredictions<<endl;
 
+        for(int i = 0; i < 7; i++){
+            singleBitBimodal[i].checkTable(address, behavior);
+            doubleBitBimodal[i].checkTable(address,behavior);
+        }
+        
+        for(int i = 0; i < 9; i++){
+             GShares[i].checkTable(address, behavior);
+        }
+        
+
+
+        
+    }
+    /*********************************************************************/
+    //OUTPUT
+    ofstream f(OUTPUTFILEPATH);
+    if(f){
+        //Always Taken / Always Non Taken
+        f << atpredictions <<","<< branches << ";"<< endl;
+        f << antpredictions <<","<< branches << ";"<< endl;
+        //Single Bit Bimodal
+        for(int i = 0; i < 6; i++){
+            f << singleBitBimodal[i].correct << ","<< branches << "; ";
+        }
+        f << singleBitBimodal[6].correct << ","<< branches << ";"<<endl;
+        //Two Bit Saturating Bimodal
+        for(int i = 0; i < 6; i++){
+            f <<doubleBitBimodal[i].correct << ","<< branches << "; ";
+        }
+        f << doubleBitBimodal[6].correct << ","<< branches << ";"<<endl;
+        //Gshare
+        for(int i = 0; i < 8; i++){
+            f <<GShares[i].correct << ","<< branches << "; ";
+        } 
+        f << GShares[8].correct << ","<< branches << ";"<<endl;
+        
+    }
+    f.close();
 }
 
 int main(int argc, char *argv[]){
--- project1/examples/predictors.h
+++ project1/examples/predictors.h
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+
 void alwaysTaken(unsigned long long , std::string , unsigned long long , int &);
 void alwaysNonTaken(unsigned long long , std::string , unsigned long long , int &);
 void predictor();
@@ -15,11 +16,13 @@ public:
     std::vector<int> table;
     int tablesize;
     int bits;
+    int correct;
+
     Bimodal(){
-        table.resize(0);
-        bits = 0;
-        tablesize = 0;
-        std::cout << "empty"<<std::endl;
+        this->table.resize(0);
+        this->bits = 0;
+        this->tablesize = 0;
+        correct = 0;
     }
     Bimodal(unsigned size, unsigned num_bits){
     /*
@@ -31,79 +34,123 @@ public:
     */
         this->bits = num_bits;
         this->tablesize = size;
-        table.resize(tablesize);
-        if(bits == 1){
-            //Initiallize all entries to Taken
-            for(int i = 0; i < tablesize; i++){
-                table[i] == 3;
-            }
-        }
-        else if(bits == 2){
-            //Initiallize all entries to Strongly Taken
-            for(int i = 0; i < tablesize; i++){
-                table[i] == 3;
-            }
-        }
-        else{
-            std::cout << "err with bit num" <<std::endl;
-        }
+        this->correct = 0;
+        this->table =  std::vector<int>(tablesize, 3);
     }
-    bool checkTable(int entry_number, std::string behavior, int & correct){
-        if(behavior != "T" && behavior != "NT") std::cout<< "incorrect behavior"<<std::endl;
-        //updates if wrong//
+    int checkTable(unsigned long long index, std::string behavior){
+        //check and modify table
+        /* Returns the Prediction based on the following:
+        *  -1 = err
+        *  1 = Taken
+        *  0 = Non Taken
+        */
+        unsigned int entry_number = index % this->tablesize;
+        if(behavior != "T" && behavior != "NT"){
+            std::cout<< "incorrect behavior"<<std::endl;
+            return -1;
+        }
         // 0 <= entry <= tablesize
         if(bits == 1){
-            //TAKEN PREDICTION
+            //Correct TAKEN PREDICTION
             if(behavior == "T" && table[entry_number] == 3){
                 correct ++;
-                return true;
-            }//NON TAKEN PREDICTION
+                return 1;
+            }//InCorrect NON TAKEN PREDICTION
             else if(behavior == "T" && table[entry_number] == 0){
                 table[entry_number] = 3;
-                return false;
-            }//TAKEN PREDICTION
+                return 0;
+            }//InCorrect TAKEN PREDICTION
             else if(behavior == "NT" && table[entry_number] == 3){
                 table[entry_number] = 0;
-                return false;
-            }//NON TAKEN PREDICTION
+                return 1;
+            }//Correct NON TAKEN PREDICTION
             else if(behavior == "NT" && table[entry_number] == 0){
                 correct++;
-                return true;
+                return 0;
             }
-            else return false;
+            else{
+                /*std::cout<<"Bug in Code, index = "<< index<<", entry no = "<<entry_number<< 
+                ", BITS = " << this->bits <<", SIZE = "<<this->tablesize <<", VALUE = " <<table[entry_number]<<
+                ", BEHAVIOR = "<< behavior << std::endl;*/
+                return -1;
+            } 
         }
         else if(bits == 2){
             int prediction = table[entry_number];
-            //TAKEN PREDICTION
+            if(prediction < 0) std::cout<<"ERR, NEGATIVE PREDICTION"<<std::endl;
+            //Correct TAKEN PREDICTION
             if(prediction >=2 && behavior == "T"){ 
+                //std::cout << "CORRECT" << std::endl;
                 correct++;
                 table[entry_number] = 3; //go to strongly taken if not already
-                return true; 
-            }//NON TAKEN PREDICTION
-            if(prediction < 2 && behavior == "NT"){
+                return 1; 
+            }//Correct NON TAKEN PREDICTION
+            else if(prediction < 2 && behavior == "NT"){
+                //std::cout << "CORRECT" << std::endl;
                 correct++;
                 table[entry_number] = 0;
-                return true;
+                return 0;
             }
-            //incorrect prediction
-            if(behavior == "T"){
+            //InCorrect TAKEN PRECICTION
+            else if(prediction < 2 && behavior == "T"){
                 if(table[entry_number] == 3) std::cout << "prediction err"<<std::endl;
-                else table[entry_number] = table[entry_number] + 1;
+                else table[entry_number] += 1;
+                return 1;
             }
-            else if (behavior == "NT"){
+            //InCorrect NONTAKEN PRECICTION
+            else if (prediction >= 2 && behavior == "NT"){
                 if(table[entry_number] == 0) std::cout << "prediction err"<<std::endl;
-                else table[entry_number] = table[entry_number] - 1;
+                else table[entry_number] -= 1;
+                return 0;
             }
-            return false;
+            else{ std::cout<<"err line 95"<<std::endl;}
+            return -1;
         }
         else{
             std::cout <<  "err with bit num" <<std::endl;
         }
-        return false;
+        return -1;
     }
     
 
 };
+class GShare{
+public:    
+    Bimodal table;
+    int history_bits;
+    int correct;
+    std::string history_register; //store register as string for length enforcement
+    
 
-
+    GShare(){
+        this->history_bits = 0;
+        correct = 0;
+    }
+    GShare(unsigned history_bits){
+        history_register = "";
+        for(int i = 0; i < history_bits; i++) {history_register.append("0");}
+        this->history_bits = history_bits;
+        int tablesize;
+        correct = 0;
+        this->table =  Bimodal(2048, 2);
+    }
+    int checkTable(unsigned long long address, std::string behavior){
+        //check and modify table
+        /* Returns the Prediction based on the following:
+        *  -1 = err
+        *  1 = Taken
+        *  0 = Non Taken
+        */
+        unsigned long long index = (address ^ stoi(history_register, nullptr, 2)); //xor address with register to get index
+        int prediction = this->table.checkTable(index, behavior);
+        // if branch taken, shift left and store a 1, else 0
+        if(prediction == 1){
+            history_register = history_register.substr(1, history_register.size()) + "1";
+        }else{
+            history_register = history_register.substr(1, history_register.size()) + "0";
+        }
+        this->correct = this->table.correct;
+        return prediction;
+    }
+};
 #endif
