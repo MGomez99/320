ebfc061e7b5276feeeee721728619a7b72c568da
MGomez99 <xmgomez2012@gmail.com>
2019-11-19 23:03:19 -0500

    dm cache is donem sa wip

--- project2/cache-sim.cpp
+++ project2/cache-sim.cpp
@@ -6,37 +6,135 @@ using namespace std;
 for caches that need to use LRU policy, have a counter per entry that keeps track of time ticks (increment when not accessed, reset to 0 when accessed)
 sort entries/vector/whatever in ascending order? or have aux function to grab LRU entry
 */
+string direct(int size, vector<trace> traces){
+  int hits = 0, misses = 0, accesses = 0;
+  int entries = size/32;
+
+  vector<Block> table(size, Block(0, 0));
+  //cout << "Size of traces: " << traces.size() << " size of table: " << table.size() <<endl;
+  for(trace t: traces){
+    if(t.target == 0){
+      cout << "0 address" << endl;
+      continue;
+    }
+    unsigned index = (t.target >> 5) % entries;
+    unsigned tag = t.target >> 5+ size;
+    //cout << "Target "<< t.target << " Instr: " << t.instruction <<" Index: " <<index << " Tag: " << tag<< endl;
+
+    Block & currentBlock = table[index];
+    //cout<< " Current Tag: " << currentBlock.tag << " Valid: " << currentBlock.valid_bit<<endl;
+    accesses++;
+    if(currentBlock.valid_bit == 0 || currentBlock.tag != tag){
+      //cout << "miss"<<endl;
+      misses++;
+      currentBlock.valid_bit = 1;
+      currentBlock.tag = tag;
+    }
+    else{
+      //cout << "hit" <<endl;
+      hits++;
+    }
+
+  }
+  return IntToString(hits)+","+IntToString(accesses)+";";
+}
+
+string set_associative(int set_size, vector<trace> traces, int write_miss = 0, int NL_prefetching = 0){
+  int env_setting = 0;
+  if(write_miss && !NL_prefetching) env_setting = 1;
+  if(!write_miss && NL_prefetching) env_setting = 2;
+  if(write_miss && NL_prefetching) env_setting = 3;
+  //dimensions
+  //set size = blocks per set; blocks = lines
+  unsigned cache_size = pow(2, 14), block_size = 32; //total cache size is 16KB = 2^14, block size
+  unsigned total_entries = cache_size / block_size; //cache lines
+  unsigned num_sets = total_entries / set_size; //number of sets
+  unsigned entries_per_set = total_entries / num_sets; //lines per set
+
+  //bit break down
+  unsigned size_offset = log2(block_size); // 5
+  unsigned size_index = log2(num_sets);
+  cout<< "cache size: " << cache_size << " num sets: " << num_sets << " entries per set: " << entries_per_set << " "<<endl;
+  //for output
+  int hits = 0, misses = 0, accesses = 0;
+  shift_amnt_SA = size_index + size_offset;
+  //cache will be represented by a vector of sets, each set contains n entries where n = set_size
+  vector<Set> table(num_sets, Set(set_size));
+  
+  //cout << "Size of traces: " << traces.size() << " size of table: " << table.size() <<endl;
+  for(trace t: traces){
+    if(t.target == 0){
+      cout << "0 address" << endl;
+      continue;
+    }
+    accesses++;
+    unsigned index = t.target >> size_offset;
+    unsigned current_set_number = index % num_sets;
+    unsigned tag = index >> size_index;
+    Set & currentSet = table[current_set_number];
+
+    if(inSet(currentSet, t.target)){
+      //cout << "hit!" <<endl;
+      hits++;
+      incrementLRUCounters(currentSet, t.target);
+      if(env_setting == 2){
+        //prefetch next line always (env setting 2)
+
+      }
+    }
+    else{
+      //cout << "miss!" <<endl;
+      misses++;
+      //normal miss, fetch line
+      if(env_setting == 0 || env_setting == 1){
+        //if no alloc on write miss is set, we can't write on store instructions
+        if(env_setting == 0 || t.instruction != "S") fetchLine(currentSet, t.target);
+      }
+      //next line prefetching is enabled
+      else {
+        fetchLine(currentSet, t.target);
+        //@TODO fetch next line
+      }
+     
+      incrementLRUCounters(currentSet, t.target);
+    }
+    
+    
+
+  }
+  shift_amnt_SA = 0;
+  return IntToString(hits)+","+IntToString(accesses)+";";
+}
+
 int cache_sim(string inputfile, string outputfile){
   vector<trace> traces = parseFile(inputfile, outputfile);
-  cout << "traces parsed from file" <<endl;
   //Direct Mapped inititalization
-  int dm_sizes[4] = {10, 12, 14, 15}; //1KB = 2^10, 4KB = 2^ 12, 16KB = 2^14, 32 KB = 2^15
-  vector<DM_Cache> DMs;
-  DMs.resize(4);
-  for(int i = 0; i < 4; i++){
-    DMs[i] = DM_Cache(pow(2, dm_sizes[i]));
-  }
-
-  //test all caches
-  cout << "testing caches" <<endl;
-  cout << "line 1 things:  " << traces[0].instruction << traces[0].target << endl;
-  for(int i = 0; i < 4; i++){
-    for(int j = 0; j < traces.size(); j++){
-      DMs[i].updateCache(traces[j]);
-    }
-  }
   
-  cout << "done" <<endl;
-
-  //print results
+  vector<string> output (0);
+  cout << "Direct Mapped" << endl;
+  /*** DIRECT MAPPED CACHE ***/
+  int dm_sizes[4] = {10, 12, 14, 15}; //1KB = 2^10, 4KB = 2^ 12, 16KB = 2^14, 32 KB = 2^15
+  string l1 = "";
   for(int i = 0; i < 4; i++){
-    cout << DMs[i].cache->hits << ","<<DMs[i].cache->accesses <<";";
-    cout << endl;
+    l1.append(direct((int)pow(2, dm_sizes[i]), traces));
+    if(i != 3) l1.append(" ");
+  } 
+  output.push_back(l1);
+  /**************************/
+  cout << "Set Associative"<<endl;
+  /** SET ASSOCIATIVE CACHE**/
+  string l2 = "";
+  int sa_ways[4] = {2, 4, 8, 16};
+  for(int i = 0; i < 4; i++){
+    l2.append(set_associative(sa_ways[i], traces, 0, 0));
+    if(i != 3) l2.append(" ");
   }
-  return 0;
+  cout<< l2<<endl;
+  output.push_back(l2);
+  return writeResults(output, outputfile);
    
 }
 
 int main(int argc, char *argv[]){
-    return cache_sim(argv[1], argv[2]);
+  return cache_sim(argv[1], argv[2]);
 }
--- project2/cache-sim.h
+++ project2/cache-sim.h
@@ -10,57 +10,43 @@
 #include <utility>
 #include <cmath>
 
-#define BLOCKSIZE 32
 using namespace std;
+int shift_amnt_SA = 0;
 
-/*
-//example from class
-cache with 2 entries, 4 blocks per entry
-
-address: 38
-binary: 100110
-
-index = (address / blocks) % entries
-= (38 / 4) % 2 
-= 1
-address binary: 100110
-
-the first two bits in the binary is ignored because that's the offset to get to the 3rd block (00, 01, 10, 11)
-so the index resides in the 3rd bit, which is 1.
-
-the tag is the rest of the data that isn't the offset or index, which is binary: 100 in this case
-
-tag = (address / blocks) / entries
-= (38 / 4) / 2
-= 9 / 2
-= 4 binary: 100
-*/
 /***************************************/
 /*               structs               */
 /***************************************/
 struct trace{
   
   unsigned long long target;
-  string instruction;
-  trace(unsigned long long tg, string instr): target(tg), instruction(instr){}
+  std::string instruction;
+  trace(unsigned long long tg, std::string instr): target(tg), instruction(instr){}
   trace(){target = 0; instruction = "";}
 };
 struct Block{
   unsigned valid_bit = 0;
   unsigned long long tag = 0;
-  Block(unsigned valid, unsigned long long tg): valid_bit(valid), tag(tg) {}
-  Block(){valid_bit = 0; tag = 0;}
+  unsigned count = 0;
+ unsigned long long target = 0;
+  Block(unsigned valid, unsigned long long tg, unsigned ct = 0, unsigned long long trg = 0): valid_bit(valid), tag(tg), count(ct), target(trg){}
+  Block(){valid_bit = 0, tag = 0, count = 0, target = 0;} //removed 0s
 };
 
-class Cache{
-  public:
-  int hits, misses, accesses, cache_size, entries;
-  vector<Block> table;
-  Cache(){
-    hits = 0, misses = 0, accesses  = 0, cache_size = 0, entries = 0;
-    table.resize(0);
+struct Set{
+  unsigned size = 0;
+  vector<Block> * ptr;
+  vector <Block> set;
+  Set(unsigned s){
+    size = s;
+    ptr = new vector<Block>(s, Block(0, 0, 0));
+    set = *ptr;
+    
   }
- 
+  Set(){
+    size = 0; ptr = 0; set.resize(0);
+  }
+  ~Set(){ delete &ptr;}
+
 };
 /***************************************/
 /***************************************/
@@ -68,9 +54,73 @@ class Cache{
 /***************************************/
 /*                misc.                */
 /***************************************/
-//int log2(int n){
-  //return (int) (log(n)/log(2));
-//}
+
+string IntToString(int a)
+{
+  ostringstream temp;
+  temp << a;
+  return temp.str();
+}
+
+bool inSet(Set s, unsigned long long target){
+  if(shift_amnt_SA == 0) cout <<"ERR"<<endl;
+  for(Block b : s.set){
+    if(b.tag << shift_amnt_SA == target << shift_amnt_SA) return true;
+  }
+  return false;
+}
+
+unsigned indexToEvict(Set s, unsigned long long target){
+  int flag = 0;
+  unsigned index_LRU = -1;
+  int highest_count = -1;
+  for(int i = 0; i < s.set.size(); i++){
+    if(s.set[i].valid_bit == 0) return i;
+    //find first possible eviction candidate
+    if(!flag){
+      if(s.set[i].tag << shift_amnt_SA != target << shift_amnt_SA){
+        flag = 1;
+        index_LRU = i;
+        highest_count = s.set[index_LRU].count;
+      }
+    }
+    //find a more qualified candidate (can't be the index containing the current instruction's target's tag [useful for prefetching])
+    else if(s.set[i].count > highest_count && s.set[i].tag != target){
+      index_LRU = i;
+      highest_count = s.set[index_LRU].count;
+    }
+  }
+  return index_LRU;
+}
+void incrementLRUCounters(Set & s, unsigned long long target, unsigned long long prefetch = 0){
+  //cout<< "++ size of s is "<< s.set.size()<<"/ " << s.size <<"...";
+  int flag = 0;
+  if(prefetch == 0){
+    for(int i = 0; i < s.set.size(); i++){
+      
+      if(s.set[i].tag == target){
+        //cout<<"...target! index is "<<i <<"...";
+        flag = 1;
+        s.set[i].count = 0;
+      }
+      else{
+        //if(flag) cout<< "...kept going...";
+        s.set[i].count++;
+      }
+    }
+  }
+  //cout<<"...done"<<endl;//@TODO increment on prefetch [account for prefetched line]
+}
+
+void fetchLine(Set & s, unsigned long long target, unsigned long long prefetch = 0){
+  //fetches line on a miss / prefetches too; sets counters to 0 for LRU
+  if(prefetch == 0){
+    int index = indexToEvict(s, target);
+    s.set[index].count = 0;
+    s.set[index].tag = target;
+    s.set[index].valid_bit = 1;
+  }//@TODO prefetching
+}
 /***************************************/
 /***************************************/
 vector<trace> parseFile(string inputfile, string outputfile){
@@ -98,60 +148,15 @@ vector<trace> parseFile(string inputfile, string outputfile){
   return traces;
 }
 
-class DM_Cache{
-  public:
-  Cache* cache;
-  //number of bits for offset, index, and tag
-  int size_offset = 0;
-  int size_index = 0;
-  int size_tag = 0;
-  
-  DM_Cache(int c_size){
-    cache = new Cache();
-    cache->cache_size = c_size;
-    size_offset = 5; //log2(32)
-    size_index = log2(c_size);
-    
-    size_tag = 32 - 5 - size_index;
-    cache->table.resize(c_size);
-    cache->entries = cache->cache_size / 32;
+int writeResults(vector<string> output, string outputfile){
+  ofstream outfile(outputfile);
+  if(!outfile){
+    cout<< "Output File can't be created" << endl;
+    return -1;
   }
-  DM_Cache(){
-    cache = new Cache();
-  }
-  ~DM_Cache(){
-    delete cache;
-  }
-  void updateCache(struct trace t){
-    if(t.target == 0){
-      cout << "0 address" << endl;
-      return;
-    }
-    //calculate tag and index
-    int idx = (t.target >> 5) % cache->entries; // index = address/block size % entries
-    int tag_t = t.target >> size_offset + size_index; //tag = address >> offsetbits
-    cout << "Target "<< t.target << " Instr: " << t.instruction <<" Index: " <<idx << " Tag: " << tag_t<< endl;
-    //index into table
-    Block & current = cache->table[idx];
-    cout<< " Current Tag: " << current.tag << " Valid: " << current.valid_bit<<endl;
-    cache->accesses++;
-    //line/block not valid
-    if(current.valid_bit == 0){
-      cout << " bit isnt valid" << endl;
-      cache->misses ++;
-      current.valid_bit = 1;
-      current.tag = tag_t;
-    }//block is valid, but tag doesn't match
-    else if (current.tag != tag_t){
-      cout << " tag doesnt match" << endl;
-      cache->misses++;
-      current.tag = tag_t;
-    }//tag matches
-    else{
-      cout << " hit!" << endl;
-      cache->hits++;
-    }
-  }
+  for(string l : output) outfile << l << endl;
+  outfile.close();
+  return 0;
+}
 
-};
 #endif
